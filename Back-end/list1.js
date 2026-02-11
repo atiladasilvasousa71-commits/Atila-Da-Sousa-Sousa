ea883cf17edc2ed535aa3c179b3a97bb6a66b553
atiladasilvasousa71-commits <atiladasilvasousa71@gmail.com>
2026-02-11 08:31:35 -0300

    Update list1.js

--- Back-end/list1.js
+++ Back-end/list1.js
@@ -1,14 +1,20 @@
 **1.** O que é o Node.js?
-
+Node.js é um ambiente de execução (runtime) JavaScript de código aberto e multiplataforma que permite executar código JavaScript fora do navegador, principalmente no lado do servidor (back-end).
+  
 **2.** O Node.js usa qual linguagem de programação?
-
+ JavaScript. 
+  
 **3.** O Node.js roda no navegador ou fora dele?
-
+O Node.js roda fora do navegador
+  
 **4.** Para que serve o Node.js?
+  O Node.js serve primordialmente como um ambiente de execução (runtime) para rodar código JavaScript no lado do servidor (backend), permitindo criar aplicações de rede rápidas, escaláveis e em tempo real.
 
 **5.** Qual comando é usado para executar um arquivo Node.js no terminal?
-
+O comando usado para executar um arquivo Node.js no terminal é node, seguido pelo nome do arquivo. 
+  
 **6.** O que é o **npm**?
+O npm (Node Package Manager) é o gerenciador de pacotes padrão para o ambiente de execução Node.js. 
 
 **7.** O que significa a palavra **módulo** no Node.js?
 
